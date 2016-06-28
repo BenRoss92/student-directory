f35fc5b4bc79163d91c1348e996b7e99bf29fbc1
Ben Ross <benjaminjackross@gmail.com>
2016-06-28 13:13:03 +0000

    Can now save student data to students.csv file

--- section_9/interactive_menu.rb
+++ section_9/interactive_menu.rb
@@ -2,16 +2,23 @@
 
 def input_students
   puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
   #get the first name
   name = gets.chomp
+  #get the first cohort
+  puts "Please enter their cohort"
+  cohort = gets.chomp
+  puts "To finish, just hit return twice"
   #while the name is not empty, repeat this code
   while !name.empty? do
       #add the student hash to the array
-      @students << {name: name, cohort: :november}
+      @students << {name: name, cohort: cohort}
       puts "Now we have #{@students.count} students"
       #get another name from the user
+      puts "Please enter another name"
       name = gets.chomp
+      puts "Please enter another cohort"
+      #get another cohort from the user
+      cohort = gets.chomp
   end
 end
 
@@ -27,6 +34,7 @@ end
 def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
+    puts "3. Save the list to students.csv"
     puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -42,6 +50,8 @@ def process(selection)
         input_students
     when "2"
         show_students
+    when "3"
+        save_students
     when "9"
         exit
     else
@@ -62,6 +72,18 @@ end
 
 def print_footer
   puts "Overall, we have #{@students.count} great students"
+end
+
+def save_students
+    # open the file for writing ('w' opens it in write-only mode)
+    file = File.open("students.csv", "w")
+    # iterate over the array of students
+    @students.each do |student|
+        student_data = [student[:name], student[:cohort]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+    end
+    file.close
 end
 
 interactive_menu
