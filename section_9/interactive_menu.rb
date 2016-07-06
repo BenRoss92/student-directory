e55f15249a9aa3816a9309621b152a36183c21b0
Ben Ross <benjaminjackross@gmail.com>
2016-07-06 13:17:10 +0000

    Section 14 Q6

--- section_9/interactive_menu.rb
+++ section_9/interactive_menu.rb
@@ -4,8 +4,8 @@ def print_menu
   puts "Please choose from the following options:"
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to an exisiting file"
-  puts "4. Load the list from an exisiting file"
+  puts "3. Save the list to an existing file"
+  puts "4. Load a list from an existing file"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -82,45 +82,33 @@ def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
+# 'file_to_save' specified by user input during 'process' method
 def save_students(file_to_save)
-    # open the file for writing ('w' opens it in write-only mode)
-    # replace 'students.csv' with any file and write into it
-    file = File.open(file_to_save, "w")
+  # Opening & closing file non-manually using code block
+  File.open(file_to_save, "w") do |file|
     # iterate over the array of students
     @students.each do |student|
-        student_data = [student[:name], student[:cohort]]
-        csv_line = student_data.join(",")
-        file.puts csv_line
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
     end
-    file.close
-    puts "#{@students.count} students now saved in #{file_to_save}"
+  # No need for file.close as file closes when block ends    
+  end
+  puts "#{@students.count} students now saved in #{file_to_save}"
 end
 
-# 'filename' specified by user input during 'process' method
+# 'file_to_load' specified by user input during 'process' method
 def load_students(file_to_load)
-    # loads students in file specified by user
-  file = File.open(file_to_load, "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort}
+  # Opening & closing file non-manually using code block
+  File.open(file_to_load, "r") do |file|
+    file.readlines.each do |line|
+      name, cohort = line.chomp.split(',')
+      @students << {name: name, cohort: cohort}
+    end
+    # No need for file.close as file closes when block ends
   end
-  file.close
   puts "Loaded #{@students.count} students from #{file_to_load}"
 end
-
-=begin
-# As files loaded & saved now specified, method below isn't needed
-def try_load_students
-  filename = ARGV.first# first argument from the command line
-  return load_students if filename.nil? # get out of the method if it isn't given
-  if File.exists?(filename) # if it exists
-    load_students(filename)
-  else # if it doesn't exist
-    puts "Sorry, #{filename} doesn't exist."
-    exit # quit the program
-  end
-end
-=end
 
 interactive_menu
 
