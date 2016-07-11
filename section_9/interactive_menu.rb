677dbd3133312a0986797d813ce2339bf335e1fa
Ben Ross <benjaminjackross@gmail.com>
2016-07-11 15:48:08 +0000

    Section 14 Q7

--- section_9/interactive_menu.rb
+++ section_9/interactive_menu.rb
@@ -1,7 +1,9 @@
+  # Load csv library/gem - need to require 'csv' in program
+  require 'csv'
 @students = [] # an empty array accessible to all methods
 
 def print_menu
-  puts "Please choose from the following options:"
+  puts "\nPlease choose from the following options:\n\n"
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to an existing file"
@@ -10,7 +12,7 @@ def print_menu
 end
 
 def input_students
-  puts "Please enter the names of the students"
+  puts "\nPlease enter the names of the students"
   #get the first name
   name = STDIN.gets.chomp
   #get the first cohort
@@ -21,12 +23,12 @@ def input_students
       cohort = STDIN.gets.chomp
       puts "To finish, just hit return twice"
       @students << {name: name, cohort: cohort}
-      puts "Now we have #{@students.count} students"
+      puts "\nNow we have #{@students.count} students"
       #get another name from the user
-      puts "Please enter another name"
+      puts "Please enter another name or hit return twice to finish"
       name = STDIN.gets.chomp
   end
-  puts "#{@students.count} students stored in memory - press option 3 to save to disk"
+  puts "#{@students.count} students stored in memory - choose option 3 to save to disk"
 end
 
 def interactive_menu
@@ -52,23 +54,23 @@ def process(selection)
         show_students
     when "3"
         # Ask user which file to save
-         puts "Which file would you like to save these students in?"
+         puts "\nWhich file would you like to save these students in?"
          # gets.chomp is the argument then used in 'save_students' method
         save_students(STDIN.gets.chomp)
     when "4"
         # Ask user which file to load
-        puts "What file would you like to load?"
+        puts "\nWhat file would you like to load?"
         # gets.chomp is the argument then used in 'load_students' method
         load_students(STDIN.gets.chomp)
     when "9"
         exit
     else
-        puts "I don't know what you mean, try again"
+        puts "\nI don't know what you mean, please try again"
     end
 end
 
 def print_header
-  puts "The students of Villains Academy"
+  puts "\nThe students of Villains Academy"
   puts "-------------"
 end
 
@@ -79,35 +81,38 @@ def print_students_list
 end
 
 def print_footer
-  puts "Overall, we have #{@students.count} great students"
+  puts "\nOverall, we have #{@students.count} great students"
 end
 
+# NEW SAVE METHOD WORKS
 # 'file_to_save' specified by user input during 'process' method
 def save_students(file_to_save)
-  # Opening & closing file non-manually using code block
-  File.open(file_to_save, "w") do |file|
-    # iterate over the array of students
+  # open & write ('a+' - append on next line) to file using CSV library & code block
+    CSV.open(file_to_save, "a+") do |file|
+    # iterate over the array of arrays
     @students.each do |student|
       student_data = [student[:name], student[:cohort]]
-      csv_line = student_data.join(",")
-      file.puts csv_line
+      # csv_line = student_data.join(",")
+      #file.puts csv_line
+      file.puts student_data
     end
   # No need for file.close as file closes when block ends    
   end
-  puts "#{@students.count} students now saved in #{file_to_save}"
+    puts "\nSaved #{@students.count} students to #{file_to_save} file"
 end
 
 # 'file_to_load' specified by user input during 'process' method
 def load_students(file_to_load)
-  # Opening & closing file non-manually using code block
-  File.open(file_to_load, "r") do |file|
-    file.readlines.each do |line|
-      name, cohort = line.chomp.split(',')
+  # Open & close file non-manually using CSV library and code block
+  CSV.open(file_to_load, "r") do |file|
+    file.readlines.map do |array|
+      # get name & cohort from array of arrays using parallel assignment
+      name, cohort = array[0], array[1]
       @students << {name: name, cohort: cohort}
-    end
+      end
     # No need for file.close as file closes when block ends
   end
-  puts "Loaded #{@students.count} students from #{file_to_load}"
+  puts "\nLoaded #{@students.count} students from #{file_to_load}"
 end
 
 interactive_menu
